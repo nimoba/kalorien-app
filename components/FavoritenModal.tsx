e2aebbe39241249fcde36c2191669730ec3ed797
nimoba <niklas.bartruff@gmail.com>
2025-08-19 22:02:44 +0200

    Fix Favorite Search

--- components/FavoritenModal.tsx
+++ components/FavoritenModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect, useMemo, useCallback } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export interface FavoritItem {
   name: string;
